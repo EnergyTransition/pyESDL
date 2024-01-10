591cf913e8f430563aaa4dcbba983ea507285581
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2024-01-10 14:28:57 +0100

    Trying to get automatic API docs generation working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../../esdl"))
+sys.path.insert(0, os.path.abspath("../../esdl/"))
 sys.path.insert(0, os.path.abspath("../../"))
 print(sys.path)
 
